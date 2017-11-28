841fd50ddffcdd87a46d5e1de8058ff65cd97dc4
shikto1 <islamahidul1@gmail.com>
2017-11-28 23:51:32 +0600

    // shishir

--- app/src/main/java/com/example/shishir/locationhack/Activity/MapsActivity.java
+++ app/src/main/java/com/example/shishir/locationhack/Activity/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.shishir.locationhack.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.shishir.locationhack.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
